a2097a84bc8117317103aa84503529873dbf7daa
Loke Gudmann <loke@fiberflex.dk>
2018-11-10 14:55:09 +0100

    Added Character name setting

--- CharacterCreator/CharacterCreator/Program.cs
+++ CharacterCreator/CharacterCreator/Program.cs
@@ -10,7 +10,62 @@ namespace CharacterCreator
     {
         static void Main(string[] args)
         {
+            Character character = new Character();
+            character.CharacterNaming(character);
+        }
+    }
+
+
+    class Character
+    {
+        string name;
+        int strength;
+        int dexterity;
+        int constitution;
+        int intelligence;
+        int wisdom;
+        int charisma;
+
+        /// <summary>
+        /// Function to name the character
+        /// </summary>
+        /// <param name="character">Character to name</param>
+        public void CharacterNaming(Character character)
+        {
+            Console.WriteLine("Please enter your characters name");
+            string name = Console.ReadLine();
+            bool namePicking = true;
+            while (namePicking)
+            {
+                Console.Clear();
+                Console.WriteLine("{0}, are you sure you want {0} to be your characters name? (y/n)", name);
+                string input = Console.ReadLine();
+                if (input.ToLower() == "y" || input.ToLower() == "yes")
+                {
+                    character.name = name;
+                    namePicking = false;
+                }
+                else if (input.ToLower() == "n" || input.ToLower() == "no")
+                {
+                    Console.WriteLine("Please enter your characters name");
+                    name = Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry I didn't understand try again \n" +
+                        "Press enter to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                Console.Clear();
+            }
+        }
+
+        public void DiceRoll(Character character)
+        {
 
         }
     }
+
 }
